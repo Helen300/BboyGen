172cd9d34dc62b2088b0ac13db899458195bcc6b
Helen Chen <hc20@princeton.edu>
2020-05-19 17:20:19 -0400

    finished setting up serializers, frontend react setting up Ant Design (24:09)

--- backend/src/bboyapp/api/serializers.py
+++ backend/src/bboyapp/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from bboyapp.models import Move 
 
+# from JSON to model 
 class MoveSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Move
---
+++ frontend/bboyapp/src/containers/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomLayout = (props) => {
+	return (
+		// CUSTOM LAYOUT CODE 
+		)
+
+}
+
+// DELETE DOM THING 
+export default CustomLayout;
