82799ac618e6dd49f83cf7a54190c64dfc8abcff
unknown <danielleung27@yahoo.com>
2020-05-26 15:20:42 -0700

    Add models

--- bboyapp/models.py
+++ bboyapp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 
+class UserProfile(models.Model):
+	username = models.CharField(max_length=32)
+	added_moves = JSONField(null=True, blank=True)
 
 class Move(models.Model):
 	name = models.CharField(max_length=100)
+	description = models.TextField()
