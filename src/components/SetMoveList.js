fe686645ede4aaeb9762a761ae1a9742a701599c
unknown <danielleung27@yahoo.com>
2020-06-14 02:01:19 -0700

    Refactor editing name, add pressing enter, style changes

--- src/components/SetMoveList.js
+++ src/components/SetMoveList.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import $ from 'jquery';
-import { Tabs, Input } from 'antd';
-import { EditOutlined, CheckOutlined } from '@ant-design/icons'
+import { Tabs, Button } from 'antd';
+import { EditOutlined } from '@ant-design/icons'
 import CardList from '../components/CardList';
+import EditSetName from '../components/EditSetName';
 import { paneNames, cardTypes } from "../constants"
 
 import "../css/containers/Pane.css"
+
 const { TabPane } = Tabs;
-const { TextArea } = Input;
 
 // for all moves, there exists one moveDetail div that gets updated 
 class SetMoveList extends React.Component {
@@ -17,8 +18,6 @@ class SetMoveList extends React.Component {
 	state = {
 		setIdx: -1,
 		currentTab: 'All',
-		editingName: false,
-		currentName: "",
 	}
 
 	updateSetMoveList(newList) {
@@ -35,32 +34,6 @@ class SetMoveList extends React.Component {
 
 	}
 
-	changeSetName() {
-		var newName = this.state.currentName;
-		var newSetList = this.props.setList;
-		newSetList[this.props.selectedSetIdx].name = newName;
-		this.setState({
-			editingName: false,
-		})
-		this.props.updateSetList(newSetList);
-	}
-
-	componentWillReceiveProps(newProps) {
-		if (newProps.selectedSetIdx != -1) {
-			this.setState({
-				currentName: newProps.setList[newProps.selectedSetIdx].name,
-			})
-		}
-	}
-
-	changingName() {
-		var newName = $('#SetName').val()
-		this.setState({
-			currentName: newName,
-		})
-
-	}
-
 
 	render() {
 		if (this.props.selectedSetIdx == -1) {
@@ -73,23 +46,11 @@ class SetMoveList extends React.Component {
 			return (
 				// since we need to return one div
 				<div>
-					<div>
-						{this.state.editingName ? 
-							<div>
-							<TextArea 
-								id="SetName" 
-								rows={1}
-								value={this.state.currentName} 
-								onChange={() => this.changingName()}/>
-							<CheckOutlined onClick={() => this.changeSetName()}/>
-							</div>
-						:
-						<h4>{this.props.setList[this.props.selectedSetIdx].name}
-							<EditOutlined onClick={() => this.setState({editingName:true})}/>
-						</h4>
-
-						}
-					</div>
+					<EditSetName
+						selectedSetIdx={this.props.selectedSetIdx}
+						updateSetList={this.props.updateSetList.bind(this)}
+						setList={this.props.setList}
+					/>
 					<div className="Pane">
 						<CardList
 							cardType={cardTypes.SET_MOVE}
