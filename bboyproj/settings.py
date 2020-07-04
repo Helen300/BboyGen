d72d2b917f9509dab1a8f04894bdcd03352f83dc
unknown <danielleung27@yahoo.com>
2020-07-03 17:07:44 -0700

    test, see if this works

--- bboyproj/settings.py
+++ bboyproj/settings.py
@@ -155,7 +155,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
