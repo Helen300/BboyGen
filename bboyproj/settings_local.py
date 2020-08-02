da408977199088866119fa65c19437b639be52dd
Helen <43377525+Helen300@users.noreply.github.com>
2020-08-01 20:31:09 -0400

    Update settings_local.py
    running locally

--- bboyproj/settings_local.py
+++ bboyproj/settings_local.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+# TO RUN LOCALLY WITH THIS SETTINGS FILE
+# python manage.py runserver --settings=bboyproj.settings_local
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
